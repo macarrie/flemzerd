55a728ee20c2fb823b3a0a69113d7e3589348843
macarrie <mathieu0carrie@gmail.com>
2018-03-24 14:08:11 +0100

    Removed external modules empty test files

--- downloaders/impl/transmission/transmission_test.go
+++
@@ -1,9 +0,0 @@
-package transmission
-
-import (
-	"testing"
-)
-
-func TestNew(t *testing.T)        {}
-func TestInit(t *testing.T)       {}
-func TestAddTorrent(t *testing.T) {}
--- indexers/impl/torznab/torznab_test.go
+++
@@ -1,9 +0,0 @@
-package torznab
-
-import (
-	"testing"
-)
-
-func TestNew(t *testing.T)                  {}
-func TestGetTorrentForEpisode(t *testing.T) {}
-func TestGetName(t *testing.T)              {}
