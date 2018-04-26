2e54446dfeb9bb8b0b81ff4c7aeb2d93415c3953
macarrie <mathieu0carrie@gmail.com>
2018-04-26 23:41:51 +0200

    Removed unused fields in guessit mediainfo structs

--- objects/mediainfo.go
+++ objects/mediainfo.go
@@ -3,18 +3,12 @@ package objects
 import "github.com/jinzhu/gorm"
 
 type MediaInfo struct {
-	Type             string `json:"type"`
-	Title            string `json:"title"`
-	Container        string `json:"container"`
-	Year             int    `json:"year"`
-	ReleaseGroup     string `json:"release_group"`
-	Format           string `json:"format"`
-	ScreenSize       string `json:"screen_size"`
-	AudioChannels    string `json:"audio_channels"`
-	AudioCodecs      string `json:"audio_codecs"`
-	Language         string `json:"language"`
-	SubtitleLanguage string `json:"subtitle_language"`
-	Size             string `json:"size"`
+	Type       string `json:"type"`
+	Title      string `json:"title"`
+	Container  string `json:"container"`
+	Year       int    `json:"year"`
+	ScreenSize string `json:"screen_size"`
+	Size       string `json:"size"`
 }
 
 type EpisodeTorrentInfo struct {
