badb3b44fb219e4da92e600393adc01f95213761
macarrie <mathieu0carrie@gmail.com>
2019-07-11 14:31:51 +0200

    WIP: JWT auth

--- go.mod
+++ go.mod
@@ -4,44 +4,33 @@ require (
 	github.com/0xAX/notificator v0.0.0-20171022182052-88d57ee9043b
 	github.com/BurntSushi/toml v0.3.0 // indirect
 	github.com/Sirupsen/logrus v1.0.5 // indirect
+	github.com/appleboy/gin-jwt/v2 v2.6.2
 	github.com/coreos/go-systemd v0.0.0-20180525142239-a4887aeaa186
-	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/denisenkom/go-mssqldb v0.0.0-20180521163914-c4e5513e46d4 // indirect
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/foolin/gin-template v0.0.0-20180515083052-089e62d1d723
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
-	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-contrib/static v0.0.0-20180301030858-73da7037e716
-	github.com/gin-gonic/gin v1.3.0
-	github.com/go-sql-driver/mysql v1.3.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
 	github.com/go-telegram-bot-api/telegram-bot-api v0.0.0-20180328131029-0e0af0c480ea
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
-	github.com/golang/protobuf v1.1.0 // indirect
 	github.com/hashicorp/errwrap v0.0.0-20180715044906-d6c0cd880357 // indirect
 	github.com/hashicorp/go-multierror v0.0.0-20180717150148-3d5d8f294aa0
 	github.com/hashicorp/hcl v0.0.0-20180404174102-ef8a98b0bbce // indirect
 	github.com/jinzhu/gorm v1.9.2
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/jinzhu/now v0.0.0-20180511015916-ed742868f2ae // indirect
-	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kylelemons/go-gypsy v0.0.0-20160905020020-08cad365cd28 // indirect
-	github.com/lib/pq v0.0.0-20180523175426-90697d60dd84 // indirect
 	github.com/macarrie/transmission v0.0.0-20180521203359-2a6f6cd93675
 	github.com/magiconair/properties v1.8.0 // indirect
-	github.com/mattn/go-isatty v0.0.3 // indirect
-	github.com/mattn/go-sqlite3 v1.7.0 // indirect
 	github.com/mitchellh/mapstructure v0.0.0-20180511142126-bb74f1db0675 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ogier/pflag v0.0.1
 	github.com/onsi/ginkgo v1.5.0 // indirect
 	github.com/onsi/gomega v1.4.0 // indirect
 	github.com/pdf/kodirpc v0.0.1
-	github.com/pelletier/go-toml v1.1.0 // indirect
 	github.com/pioz/tvdb v0.0.0-20171110111739-0359b91e22a8
 	github.com/pkg/errors v0.8.0
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/xid v1.2.0
 	github.com/ryanbradynd05/go-tmdb v0.0.0-20180524153255-bbb88dee73c7
 	github.com/sirupsen/logrus v1.0.5
@@ -50,19 +39,11 @@ require (
 	github.com/spf13/jwalterweatherman v0.0.0-20180109140146-7c0cea34c8ec // indirect
 	github.com/spf13/pflag v1.0.1 // indirect
 	github.com/spf13/viper v1.0.2
-	github.com/stretchr/testify v0.0.0-20170130113145-4d4bfba8f1d1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	github.com/ugorji/go v1.1.4 // indirect
 	github.com/upgear/go-kit v0.0.0-20180517233053-4d662cc2f16f
-	golang.org/x/crypto v0.0.0-20180527072434-ab813273cd59 // indirect
-	golang.org/x/net v0.0.0-20180524181706-dfa909b99c79 // indirect
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
-	golang.org/x/sys v0.0.0-20180525142821-c11f84a56e43
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223
 	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
-	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -6,11 +6,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
-	multierror "github.com/hashicorp/go-multierror"
-
+	"github.com/hashicorp/go-multierror"
 	"github.com/macarrie/flemzerd/configuration"
+
 	log "github.com/macarrie/flemzerd/logging"
 	"github.com/macarrie/flemzerd/stats"
 )
@@ -19,11 +20,109 @@ var srv *http.Server
 var router *gin.Engine
 var serverStarted bool
 
+var identityKey = "id"
+
+type login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+type User struct {
+	UserName  string
+	FirstName string
+	LastName  string
+}
+
 func init() {
 	serverStarted = false
 }
 
+func helloHandler(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+	user, _ := c.Get(identityKey)
+	c.JSON(200, gin.H{
+		"userID":   claims["id"],
+		"userName": user.(*User).UserName,
+		"text":     "Hello World.",
+	})
+}
+
 func initRouter() {
+	// the jwt middleware
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*User); ok {
+				return jwt.MapClaims{
+					identityKey: v.UserName,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &User{
+				UserName: claims["id"].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals login
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userID := loginVals.Username
+			password := loginVals.Password
+
+			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+				return &User{
+					UserName:  userID,
+					LastName:  "Bo-Yi",
+					FirstName: "Wu",
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*User); ok && v.UserName == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		// - "param:<name>"
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+
+		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenHeadName: "Bearer",
+
+		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+		TimeFunc: time.Now,
+	})
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
 	router = gin.Default()
 	router.Use(static.Serve("/static", static.LocalFile("/var/lib/flemzerd/server/ui/static", true)))
 
@@ -36,6 +135,15 @@ func initRouter() {
 		routes.GET("/", func(c *gin.Context) {
 			c.Redirect(http.StatusMovedPermanently, "/dashboard")
 		})
+	}
+
+	auth := router.Group("/auth")
+	// Refresh time can be longer than token timeout
+	auth.GET("/refresh_token", authMiddleware.RefreshHandler)
+	auth.POST("/login", authMiddleware.LoginHandler)
+	auth.Use(authMiddleware.MiddlewareFunc())
+	{
+		auth.GET("/hello", helloHandler)
 	}
 
 	v1 := router.Group("/api/v1")
